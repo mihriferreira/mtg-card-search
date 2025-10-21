138539e6889df609a1b7dad7904d1a8610ac4f0b
MihriFerreira <125183293+mihriferreira@users.noreply.github.com>
2025-10-21 10:30:19 +0200

    Add description for mana_calculation.py
    This application allows users to import their Magic: The Gathering decks directly from Archidekt and automatically calculate mana statistics such as color distribution, average mana cost, and mana curve. It helps players analyze and balance their decks more efficiently.

---
+++ mana_calculation.py
@@ -0,0 +1,4 @@
+# This application allows users to import their Magic: The Gathering decks 
+#directly from Archidekt and automatically calculate mana statistics such as color distribution, 
+#average mana cost, and mana curve. It helps players analyze and balance their decks more efficiently.
+
